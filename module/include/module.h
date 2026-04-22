f9bd832a3194c4c045aab88e2531fb99466a31bd
Logan Tischler <logantischler0@gmail.com>
2026-04-22 10:54:20 -0400

    Rename new module

---
+++ module/include/module.h
@@ -0,0 +1,21 @@
+/** @file module.h
+ *
+ * @brief Module header
+ *
+ * @par
+ *
+ */
+
+#ifndef MODULE_H
+#define MODULE_H
+
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+#endif /* MODULE_H */
+
+/*** end of file ***/
---
+++ module/src/module.c
@@ -0,0 +1,13 @@
+/** @file module.c
+ *
+ * @brief Module source
+ *
+ * @par
+ *
+ */
+
+#include "module.h"
+
+
+
+/*** end of file ***/
